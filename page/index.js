1f20b8285a12c566e93085087a0931b3cfbcd55a
gabriel <gabrielcmoris@gmail.com>
2023-05-23 22:24:10 +0200

    show Initial text

--- page/index.js
+++ page/index.js
@@ -1,7 +1,22 @@
-import { gettext } from "i18n";
-
 Page({
   build() {
-    console.log(gettext("example in Page!!"));
+    const text = hmUI.createWidget(hmUI.widget.TEXT, {
+      x: 96,
+      y: 120,
+      w: 288,
+      h: 46,
+      color: 0xffffff,
+      text_size: 36,
+      align_h: hmUI.align.CENTER_H,
+      align_v: hmUI.align.CENTER_V,
+      text_style: hmUI.text_style.NONE,
+      text: "Gym Routines",
+    });
+
+    text.addEventListener(hmUI.event.CLICK_DOWN, (info) => {
+      text.setProperty(hmUI.prop.MORE, {
+        y: 200,
+      });
+    });
   },
 });
